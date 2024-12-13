b59aa56458f46a1d8a944771d975ddc4acff3627
Francisco Sousa <franciscoeds1987@gmail.com>
2024-12-13 16:05:03 -0300

    Criação do Layout do Componente de Filtro

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,16 +1,25 @@
 import {NgModule} from '@angular/core';
 import {AngularMaterialModule} from '../angular-material/angular-material.module';
 import {UserDetailsComponent} from './user-details/user-details.component';
+import {FilterComponent} from './filter/filter.component';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {MatButton} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    UserDetailsComponent
+    UserDetailsComponent,
+    FilterComponent
   ],
   imports: [
-    AngularMaterialModule
+    AngularMaterialModule,
+    CommonModule,
+    FormsModule,
+    MatButton
   ],
   exports: [
-    UserDetailsComponent
+    UserDetailsComponent,
+    FilterComponent
   ]
 })
 export class ComponentsModule {
