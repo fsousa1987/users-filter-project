be566efa8c5d0ac8743ebfc6da0e927ecfa35112
Francisco Sousa <franciscoeds1987@gmail.com>
2024-12-15 16:55:10 -0300

    Criação da Lógica de Filtro por Nome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,12 @@ export class AppComponent implements OnInit {
   userSelected: IUser = {} as IUser;
   showUserDetails: boolean = false;
   usersList: IUser[] = [];
+  usersListFiltered: IUser[] = [];
 
   ngOnInit() {
     setTimeout(() => {
       this.usersList = UsersList;
+      this.usersListFiltered = this.usersList;
     }, 1);
   }
 
@@ -28,5 +30,25 @@ export class AppComponent implements OnInit {
 
   onFilter(filterOptions: IFilterOptions) {
     console.log(filterOptions);
+
+    this.usersListFiltered = this.filterUsersList(filterOptions, this.usersList);
+  }
+
+  private filterUsersList(filterOptions: IFilterOptions, usersList: IUser[]): IUser[] {
+    let filteredList: IUser[];
+
+    filteredList = this.filterUsersListByName(filterOptions.name, usersList);
+
+    return filteredList;
+  }
+
+  private filterUsersListByName(name: string | undefined, usersList: IUser[]): IUser[] {
+    const NAME_NOT_TYPED = name === undefined;
+
+    if (NAME_NOT_TYPED) {
+      return usersList;
+    }
+
+    return usersList.filter((user) => user.nome.toLowerCase().includes(name.toLowerCase()));
   }
 }
